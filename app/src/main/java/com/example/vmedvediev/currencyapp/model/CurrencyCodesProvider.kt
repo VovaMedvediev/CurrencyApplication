8198ac5fdae32c287a49c755f9cc0b1285c01ce5
Volodymyr Medvediev <volodymyr.medvediev@sigma.software>
2018-07-06 17:06:39 +0300

    testing > Add server response tests

--- app/src/main/java/com/example/vmedvediev/currencyapp/model/CurrencyCodesProvider.kt
+++ app/src/main/java/com/example/vmedvediev/currencyapp/model/CurrencyCodesProvider.kt
@@ -2,7 +2,7 @@ package com.example.vmedvediev.currencyapp.model
 
 object CurrencyCodesProvider {
 
-    fun provideCurrencyRepository() : CurrencyRepository {
+    @JvmStatic fun provideCurrencyRepository() : CurrencyRepository {
         return CurrencyRepository(NetworkManager.initRetrofit())
     }
 }
